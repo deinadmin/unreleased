fa403a21b668844ba9bf9d1f9ce6b70f0838ee1f
Carl Steen <carldodo@mail.de>
2026-05-18 23:39:06 +0200

    Initial Commit

---
+++ unreleased/unreleasedApp.swift
@@ -0,0 +1,17 @@
+//
+//  unreleasedApp.swift
+//  unreleased
+//
+//  Created by Carl on 18.05.26.
+//
+
+import SwiftUI
+
+@main
+struct unreleasedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
